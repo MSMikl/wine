24d799c97c9f095645c0c1459d2f8c1e84910f4f
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-06-07 13:07:34 +0300

    excercises finished

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pprint
-
+from collections import defaultdict
 from datetime import datetime, timedelta, date
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
@@ -12,10 +11,9 @@ env = Environment(
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-production_data = pandas.read_excel('wine2.xlsx')
-wines = {}
+production_data = pandas.read_excel('wine2.xlsx', keep_default_na=False, na_values=None)
+wines = defaultdict(list)
 for line in production_data.values:
-    wines[line[0]] = wines.get(line[0], [])
     wines[line[0]].append(
                     {
             'Название': line[1],
@@ -24,31 +22,28 @@ for line in production_data.values:
             'Картинка': line[4]
          }
     )
-pprint.pprint(wines)
+min_price = production_data['Цена'].min()
 
+template = env.get_template('template.html')
 
+raw_age = date.today().year - 1920
+if raw_age % 100 in range(10, 16):
+    age = f'{raw_age} лет'
+elif raw_age % 10 in (2, 3, 4):
+    age = f'{raw_age} года'
+elif raw_age% 10 == 1:
+    age = f'{raw_age} год'
+else:
+    age = f'{raw_age} лет'
 
-# excel_df = pandas.read_excel('wine.xlsx').to_dict(orient='record')
+rendered_page = template.render(
+    age=age,
+    wines=wines,
+    min_price=min_price
+)
 
-# template = env.get_template('template.html')
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
-# raw_age = date.today().year - 1920
-# if raw_age % 100 in range(10, 16):
-#     age = f'{raw_age} лет'
-# elif raw_age % 10 in (2, 3, 4):
-#     age = f'{raw_age} года'
-# elif raw_age% 10 == 1:
-#     age = f'{raw_age} год'
-# else:
-#     age = f'{raw_age} лет'
-
-# rendered_page = template.render(
-#     age=age,
-#     wines=excel_df
-# )
-
-# with open('index.html', 'w', encoding="utf8") as file:
-#     file.write(rendered_page)
-
-# server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-# server.serve_forever()
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
